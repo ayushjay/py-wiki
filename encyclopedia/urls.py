72ecf90678431ad72853451d302fa0a0a6b6c408
Ayush J <ayushjayaswal51@gmail.com>
2025-08-02 15:26:04 +0530

    did 1 of 1

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [path("", views.index, name="index"),
                path("wiki/<str:title>/", views.entry_page, name="entry_page"),
+
+               
                path("trial/<str:name>", views.trial_page,name="trial_mine"),
                
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,11 +9,11 @@ def index(request):
                    {"entries": util.list_entries()})
 
 def entry_page(request, title):
-    title_dict = util.get_entry(title)
+    title_dict = {"title": title}
     return render(request,
-                "encyclopedia/entry.html", 
-                {"title_dict1": title_dict}
-                  )
+                "encyclopedia/entry.html", title_dict)
+
+
 # IMPORTANT LEARNING: IT IS THE CONTEXT DICTIONARY  NAME NAME NAME THAT IS PASSED INTO THE HTML PAGE FOR USE
 def trial_page(request, name):
     return render(request,
