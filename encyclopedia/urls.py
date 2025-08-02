311025e6a844668f2810a2c62c68337106b82959
Ayush J <ayushjayaswal51@gmail.com>
2025-08-02 14:47:30 +0530

    doing assignment 1

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path("", views.index, name="index"),
-               path("wiki/<title>/", views.entry_page, name="entry_page")]
+               path("wiki/<str:title>/", views.entry_page, name="entry_page"),
+               path("trial/<str:name>", views.trial_page,name="trial_mine"),
+               
+]
+
 
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,7 +9,15 @@ def index(request):
                    {"entries": util.list_entries()})
 
 def entry_page(request, title):
+    title_dict = util.get_entry(title)
     return render(request,
-                "encyclopedia/index.html", 
-                {"context": util.get_entry(title)}
+                "encyclopedia/entry.html", 
+                {"title_dict1": title_dict}
                   )
+# IMPORTANT LEARNING: IT IS THE CONTEXT DICTIONARY  NAME NAME NAME THAT IS PASSED INTO THE HTML PAGE FOR USE
+def trial_page(request, name):
+    return render(request,
+                  "encyclopedia/trial_html.html",
+                  {"name_dict": name}
+                  )
+
